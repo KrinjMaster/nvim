1a1e743564615d6ed646ecc75c7fac3a2fe38ddb
KrinjMaster <5opkasuka@gmail.com>
2024-05-10 22:54:50 +0300

    changed colorscheme

--- lua/krinj/plugins/colorscheme.lua
+++ lua/krinj/plugins/colorscheme.lua
@@ -1,63 +1,97 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "dgox16/oldworld.nvim",
+    lazy = false,
     priority = 1000,
     config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-          light = "latte",
-          dark = "mocha",
+      require("oldworld").setup({
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
         },
-        transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-        term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false, -- Force no italic
-        no_bold = false, -- Force no bold
-        no_underline = false, -- Force no underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "bold" }, -- Change the style of comments
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-        },
-        color_overrides = {
-          latte = {},
-          frappe = {},
-          macchiato = {},
-          mocha = {},
-        },
-        custom_highlights = {},
         integrations = {
+          alpha = true,
           cmp = true,
+          flash = true,
           gitsigns = true,
-          nvimtree = true,
+          hop = false,
+          indent_blankline = true,
+          lazy = true,
+          lsp = true,
+          markdown = true,
+          mason = true,
+          navic = false,
+          neo_tree = false,
+          noice = true,
+          notify = true,
+          rainbow_delimiters = true,
+          telescope = true,
           treesitter = true,
-          notify = false,
-          mini = {
-            enabled = true,
-            indentscope_color = "",
-          },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
         },
       })
 
-      vim.cmd([[colorscheme catppuccin]])
+      vim.cmd([[colorscheme oldworld]])
     end,
   },
+  -- {
+  --   "catppuccin/nvim",
+  --   name = "catppuccin",
+  --   priority = 1000,
+  --   config = function()
+  --     require("catppuccin").setup({
+  --       flavour = "mocha", -- latte, frappe, macchiato, mocha
+  --       background = { -- :h background
+  --         light = "latte",
+  --         dark = "mocha",
+  --       },
+  --       transparent_background = false, -- disables setting the background color.
+  --       show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+  --       term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  --       dim_inactive = {
+  --         enabled = false, -- dims the background color of inactive window
+  --         shade = "dark",
+  --         percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  --       },
+  --       no_italic = false, -- Force no italic
+  --       no_bold = false, -- Force no bold
+  --       no_underline = false, -- Force no underline
+  --       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+  --         comments = { "bold" }, -- Change the style of comments
+  --         loops = {},
+  --         functions = {},
+  --         keywords = {},
+  --         strings = {},
+  --         variables = {},
+  --         numbers = {},
+  --         booleans = {},
+  --         properties = {},
+  --         types = {},
+  --         operators = {},
+  --       },
+  --       color_overrides = {
+  --         latte = {},
+  --         frappe = {},
+  --         macchiato = {},
+  --         mocha = {},
+  --       },
+  --       custom_highlights = {},
+  --       integrations = {
+  --         cmp = true,
+  --         gitsigns = true,
+  --         nvimtree = true,
+  --         treesitter = true,
+  --         notify = false,
+  --         mini = {
+  --           enabled = true,
+  --           indentscope_color = "",
+  --         },
+  --         -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  --       },
+  --     })
+  --
+  --     vim.cmd([[colorscheme catppuccin]])
+  --   end,
+  -- },
   --  {
   --    "folke/tokyonight.nvim",
   --    priority = 1000, -- make sure to load this before all the other start plugins
